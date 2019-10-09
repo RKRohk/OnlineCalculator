05744067e162fedf221c7b30c3680779b8d85b02
RKRohk <rkakar2000@gmail.com>
2019-10-09 22:57:27 +0530

    operators cannot be used twice

--- calcScript.js
+++ calcScript.js
@@ -5,20 +5,30 @@ var equals = document.getElementById("btnEquals");
 var operationButtons = ['+','-','x','/'];
 var operation;
 var arg1,arg2;
-var resultGiven = false;
+var resultGiven = false,argObtained= false,operationGiven= false;
 $("button").click(function(){
     if(operationButtons.includes(this.textContent)){
+        if(!argObtained){
+            return;
+        }
         arg1 = parseFloat(textField.value);
         operation = this.textContent;
+        if(operationGiven){
+            return;
+        }
+        operationGiven = true;
     }
     if(resultGiven){
         textField.value = '';
         resultGiven = false;
     }
     textField.value += this.textContent;
+    argObtained = true;
 })
 $("#btnAC").click(function() {
     textField.value = '';
+    argObtained = false;
+    operationGiven = false;
 })
 equals.addEventListener("click",function(event){
     arg2 = textField.value.substring(textField.value.indexOf(operation)+1,textField.value.length-1);
--- operations.js
+++ operations.js
@@ -1,8 +1,11 @@
 function operations(arg1, arg2, operation){
+    console.log("HI");
     switch(operation){
         case '+': return parseFloat(arg1 + arg2).toFixed(2);
         case '-': return parseFloat(arg1 - arg2).toFixed(2);
         case 'x': return parseFloat(arg1 * arg2).toFixed(2);
         case '/': return parseFloat(parseFloat(arg1) / parseFloat(arg2)).toFixed(2);
+        default:
+            return arg1;
     }
 }
